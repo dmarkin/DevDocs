277a6560476a79ac46c82102f312ded0bb0d73d7
dreamonus <dmarkin@ua.fm>
2016-08-01 21:26:39 +0300

    Adding

---
+++ JS/Приоритеты операторов_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "DNIPROPETROVSK",
+      'continent': "EU",
+      'country': "UA",
+      'region': ""
+    },
+    'ip':"77.239.160.108"
+  }]);
+})
+//
+()
+
+;
